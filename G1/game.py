dc31f8b3b828bf8ebf1d47dfe58c30c2d092a9c6
sriram N V <sriramnvilvanathan@gmail.com>
2025-01-10 17:17:06 +0530

    Completed Basic Window and able to display an image in it

--- G1/game.py
+++ G1/game.py
@@ -1,5 +1,31 @@
 import pygame
+import sys
 
-pygame.init()
 
-screen = pygame.display.set_mode((640,480))
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('Untitled')
+        self.screen = pygame.display.set_mode((640,480))
+
+        self.clock = pygame.time.Clock()
+
+        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+
+
+
+    def run(self):
+        while True:
+
+            self.screen.blit(self.img, (100,200))
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
