e3fd72819bf3826e74d3ac1cf5ee6e5798ec8583
sriram N V <sriramnvilvanathan@gmail.com>
2025-01-10 17:29:22 +0530

    added movement mechanism for moving images up and down

--- G1/game.py
+++ G1/game.py
@@ -12,18 +12,38 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        self.img.set_colorkey((0, 0, 0)) # for transparent background of images
+
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
 
 
 
     def run(self):
         while True:
-
-            self.screen.blit(self.img, (100,200))
+            self.screen.fill((14,219,248))
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # bool are converted to 0 or 1, true = 1, false = 0. 1 - 0 = 1, 1 - 1 = 0
+            self.screen.blit(self.img, self.img_pos)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP or event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                        self.movement[1] = False
+
+            if self.movement[0]:
+                self.img_pos[1] -= 1
+            if self.movement[1]:
+                self.img_pos[1] += 1
             
             pygame.display.update()
             self.clock.tick(60)
