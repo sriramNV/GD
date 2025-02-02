cdc23304bb492afee6927e9fbd7592be1639ea39
sriram N V <sriramnvilvanathan@gmail.com>
2025-02-02 23:37:20 +0530

    added depth to the projected walls

--- g2/raycasting.py
+++ g2/raycasting.py
@@ -65,7 +65,7 @@ class Raycasting:
             proj_height = SCREEN_DIST / (depth + 0.0001)
 
             #draw walls
-            pg.draw.rect(self.game.screen, 'white',
+            pg.draw.rect(self.game.screen, color,
                         (ray * SCALE, HALF_HEIGHT - proj_height // 2, SCALE, proj_height ))
 
             ray_angle += DELTA_ANGLE
