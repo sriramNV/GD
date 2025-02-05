c48d5f0cafe218f49c43f624f729813f859f6160
sriram N V <sriramnvilvanathan@gmail.com>
2025-02-05 15:59:06 +0530

    added player getting hit scripts

--- g2/npc.py
+++ g2/npc.py
@@ -47,6 +47,8 @@ class NPC(AnimatedSprite):
     def attack(self):
         if self.animation_trigger:
             self.game.sound.npc_attack.play()
+            if random() < self.accuracy:
+                self.game.player.get_damage(self.attack_damage)
 
     def check_wall(self, x, y):
         return (x, y) not in self.game.map.world_map
--- g2/player.py
+++ g2/player.py
@@ -26,18 +26,18 @@ class Player:
             self.time_prev = time_now
             return True
 
-    # def check_game_over(self):
-    #     if self.health < 1:
-    #         self.game.object_renderer.game_over()
-    #         pg.display.flip()
-    #         pg.time.delay(1500)
-    #         self.game.new_game()
+    def check_game_over(self):
+        if self.health < 1:
+            self.game.object_renderer.game_over()
+            pg.display.flip()
+            pg.time.delay(1500)
+            self.game.new_game()
 
-    # def get_damage(self, damage):
-    #     self.health -= damage
-    #     self.game.object_renderer.player_damage()
-    #     self.game.sound.player_pain.play()
-    #     self.check_game_over()
+    def get_damage(self, damage):
+        self.health -= damage
+        self.game.object_renderer.player_damage()
+        self.game.sound.player_pain.play()
+        self.check_game_over()
 
     def single_fire_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
